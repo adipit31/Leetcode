3374fbdd01a8ab2c03390b6477d07f5b5db9ddf4
adipit31 <145926198+adipit31@users.noreply.github.com>
2024-12-13 00:19:14 +0530

    Create Valid Parantheses.cpp

---
+++ Valid Parantheses.cpp
@@ -0,0 +1,26 @@
+#include<stack>
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int>st;
+        for(char c:s)
+        {
+            if(c=='('|| c=='{'|| c=='[') {
+                st.push(c);
+            }
+            else{
+                if(st.empty()) return false;
+                char top = st.top();
+                st.pop();
+                if((c==')' && top!='(') ||
+                   (c=='}' && top!='{') ||
+                   (c==']' && top!='['))
+                   {
+                    return false;
+                   }
+            }
+
+        }
+        return st.empty();
+    }
+};
